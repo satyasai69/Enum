4085e921e524305e21b59d960eb6f10c58602fd0
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-01 14:48:47 +0530

    statuss

---
+++ enums.sol
@@ -0,0 +1,12 @@
+///SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+contract enums {
+    enum Status {
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Canceled
+    }
+    
+}

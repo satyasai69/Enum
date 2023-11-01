01e8826d8661248bf48a588ea52ee7b8ce5efeac
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-01 14:54:23 +0530

    update status

--- enums.sol
+++ enums.sol
@@ -8,5 +8,19 @@ contract enums {
         Rejected,
         Canceled
     }
-    
+    Status public status;
+    //check status 
+    function getStatus() public view returns(Status){
+        return status;
+    }
+    //update status 
+    function shipp() public {
+        status = Status.Pending;
+    }
+    function Accep() public {
+        status = Status.Accepted;
+    }
+    function Reject() public {
+        status = Status.Rejected;
+    }
 }
